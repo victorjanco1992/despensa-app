79d601b28773ae33b215001dbd5c3557a0649a3f
VictorAlejandroJanco <victorjanco1992@gmail.com>
2025-10-18 09:53:12 -0300

    Remove proxy configuration from Vite server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      }
-    }
+    port: 5173
+  },
+  preview: {
+    port: 5173
   }
 })
