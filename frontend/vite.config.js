511577c767c20949b2f07694b80a79d93825eb07
VictorAlejandroJanco <victorjanco1992@gmail.com>
2025-10-27 10:34:49 -0300

    Enhance Vite config with server and SW settings
    Added configuration for development server and Service Worker.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Configuración para el servidor de desarrollo
     port: 5173
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
+      // 1. Configuración de entrada (input) para incluir el Service Worker
+      input: {
+        main: './index.html',
+        sw: './public/service-worker.js' // Archivo del Service Worker que deseas incluir
+      },
+      // 2. Configuración de salida (output) para los "chunks" manuales
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
@@ -19,4 +26,3 @@ export default defineConfig({
     }
   }
 })
-
